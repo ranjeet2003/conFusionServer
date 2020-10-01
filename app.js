b86ed469f018e74c559ea015d2a23e5f3c4c5d9f
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-01 13:58:41 +0530

    assignment-4

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-var passport = require('passport');
-var authenticate = require('./authenticate');
-var config = require('./config');
-
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var uploadRouter = require('./routes/uploadRouter');
+const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter')
+var config = require('./config');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 const Dishes = require('./models/dishes');
+
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 // Connection URL
 const url = config.mongoUrl;
@@ -35,6 +36,7 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
 // Secure traffic only
 app.all('*', (req, res, next) => {
   if (req.secure) {
@@ -54,16 +56,37 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
-
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
 
 app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
 
+// function auth (req, res, next) {
+//   console.log(req.user);
 
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     res.setHeader('WWW-Authenticate', 'Basic');                          
+//     err.status = 401;
+//     next(err);
+//   }
+//   else {
+//         next();
+//   }
+// }
+
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -71,6 +94,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
