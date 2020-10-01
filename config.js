d2404380b780803d63091d7da021af45e0b680ba
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-10-01 13:43:52 +0530

    Passport Facebook(login with facebook)

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '424741651821374',
+        clientSecret: 'fb447744f8171816bd7e9735fb0ce818'
+    }
 }
